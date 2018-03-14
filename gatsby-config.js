46d00f0f6cfc1fec839b9fd72896640e9c20d972
jangerhard <jgschoepp@gmail.com>
2018-03-14 21:33:50 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Jan Schøpp\s Personal Page',
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        `gatsby-plugin-netlify`
+    ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Link from "gatsby-link";
+
+export default () =>
+    <div style={{color: `tomato`}}>
+        <h1>Hello Gatsby!</h1>
+        <p>What a world</p>
+        <img src="https://source.unsplash.com/random/400x200"
+             alt="" />
+        <br/>
+        <div>
+            <Link to="/page-2/">Go to Page 2</Link>
+            <br/>
+            <div>
+                <Link to="/counter/">Counter</Link>
+            </div>
+        </div>
+    </div>;
+

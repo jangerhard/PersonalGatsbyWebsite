72a63ffde37fdeef75bea3c7f3cb1399408cd449
jangerhard <jgschoepp@gmail.com>
2018-03-18 18:48:17 +0100

    Add buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './index.module.css';
+import Link from 'gatsby-link';
 
 export default () => (
 
@@ -7,12 +8,24 @@ export default () => (
         <div className={style.logoCircle}/>
 
         <div className={style.text}>
-            <svg >
+            <svg>
                 <text x={25} y={40} className={style.svgText}>
                     Jan Schøpp
                 </text>
             </svg>
             <h2>Full Stack Developer based in Oslo, Norway</h2>
+        </div>
+        <div className={style.buttons}>
+            <a href="https://www.linkedin.com/in/jangschoepp/" className={style.largeButton} data-ripple="true">
+                <h3>
+                    LinkedIn
+                </h3>
+            </a>
+            <Link to="/sauces/" className={style.largeButton} data-ripple="true">
+                <h3>
+                    Sauces
+                </h3>
+            </Link>
         </div>
     </div>
 

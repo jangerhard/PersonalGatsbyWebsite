c75f679e8d6c72121c8e5c3ec00a9d0ce0ce227c
jangerhard <jgschoepp@gmail.com>
2018-03-20 06:53:08 +0100

    Work on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,22 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import style from './index.module.css'
+import Link from 'gatsby-link';
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Jan Schøpp
-        </Link>
-      </h1>
+
+    <div className={style.header}>
+        <div className={style.bringToTop}>
+
+        </div>
+        <div className={style.links}>
+
+            <Link to="/sauces/" className={style.link} data-ripple="true">
+                <div className={style.button}>
+                    Sauces
+                </div>
+            </Link>
+        </div>
     </div>
-  </div>
-)
+);
 
 export default Header

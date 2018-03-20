b02f7686ace430114078d561070987ba82b9e118
jangerhard <jgschoepp@gmail.com>
2018-03-20 22:00:19 +0100

    Fix centering

--- src/components/Common/SVGText.js
+++ src/components/Common/SVGText.js
@@ -4,7 +4,7 @@ import style from './SVGText.module.css'
 const SVGText = ({text}) => (
 
     <svg className={style.svg}>
-        <text x={25} y={40} className={style.text}>
+        <text x={30} y={40} className={style.text}>
             {text}
         </text>
     </svg>

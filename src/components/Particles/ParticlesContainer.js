1ce1aac44335ae0b0abb1a7f9a56062d76d759d0
jangerhard <jgschoepp@gmail.com>
2018-03-21 22:17:31 +0100

    If pixels are under 500px, dont use particles

--- src/components/Particles/ParticlesContainer.js
+++ src/components/Particles/ParticlesContainer.js
@@ -2,17 +2,23 @@ import React from "react";
 import Particles from 'react-particles-js';
 import config from './config.json'
 
-const ParticlesContainer = () => (
-    <Particles
-        params={config}
-        style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            minHeight: "100vh",
-            zIndex: -1
-        }}
-    />
-);
+const ParticlesContainer = () => {
+
+    if (window.width < 500)
+        return;
+
+    return (
+        <Particles
+            params={config}
+            style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                minHeight: "100vh",
+                zIndex: -1
+            }}
+        />
+    )
+};
 
 export default ParticlesContainer;

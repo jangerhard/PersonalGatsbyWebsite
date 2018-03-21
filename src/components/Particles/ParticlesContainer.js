c94db9b835bb3eecfcdb7fb7bddcedfcb3135820
jangerhard <jgschoepp@gmail.com>
2018-03-21 22:30:15 +0100

    Revert "If pixels are under 500px, dont use particles"
    This reverts commit 1ce1aac

--- src/components/Particles/ParticlesContainer.js
+++ src/components/Particles/ParticlesContainer.js
@@ -2,23 +2,17 @@ import React from "react";
 import Particles from 'react-particles-js';
 import config from './config.json'
 
-const ParticlesContainer = () => {
-
-    if (window.width < 500)
-        return;
-
-    return (
-        <Particles
-            params={config}
-            style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                minHeight: "100vh",
-                zIndex: -1
-            }}
-        />
-    )
-};
+const ParticlesContainer = () => (
+    <Particles
+        params={config}
+        style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            minHeight: "100vh",
+            zIndex: -1
+        }}
+    />
+);
 
 export default ParticlesContainer;

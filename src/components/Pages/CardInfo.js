6297e6fac47bac9a9589da4b1593954a8f264dfd
Jan Schøpp <jan.gerhard.schopp@nordea.no>
2018-03-23 14:47:10 +0100

    Update using props

--- src/components/Pages/CardInfo.js
+++ src/components/Pages/CardInfo.js
@@ -1,13 +1,16 @@
 import React from "react";
 import styles from './CardInfo.module.css'
 
-const CardInfo = ({title, text}) => {
-    return (
-        <div className={styles.card_info}>
-            <h2>{title}</h2>
-            <p>{text}</p>
-        </div>
-    );
+const CardInfo = () => (
+    <div className={styles.card_info}>
+        <h2>{this.props.title}</h2>
+        <p>{this.props.text}</p>
+    </div>
+);
+
+CardInfo.propTypes = {
+    title: PropTypes.string,
+    text: PropTypes.string,
 };
 
 export default CardInfo;
--- src/components/Pages/CardPage.js
+++ src/components/Pages/CardPage.js
@@ -1,24 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import styles from './CardPage.module.css';
+import CardInfo from "./CardInfo";
 
-const CardPage = ({ title, imgLink }) => {
-    return (
-        <div className={styles.card}>
-            <div className={styles.card_top}>
-                <h1 className={styles.display1}>{title}</h1>
-            </div>
+const CardPage = () => (
+    <div className={styles.card}>
+        <div className={styles.card_top}>
+            <h1 className={styles.display1}>{this.props.title}</h1>
+        </div>
 
-            <hr />
+        <hr />
 
-            {imgLink &&
-            <div className={styles.card_image}>
-                <img className={styles.img}
-                     src={imgLink} />
-            </div>
-            }
+        {this.props.imgLink &&
+        <div className={styles.card_image}>
+            <img className={styles.img}
+                 src={this.props.imgLink} />
+        </div>
+        }
 
-            {this.props.children()}
-        </div>);
+        {this.props.children()}
+    </div>);
+
+CardPage.propTypes = {
+    title: PropTypes.string,
+    imgLink: PropTypes.string,
+    children: PropTypes.instanceOf(CardInfo),
 };
 
 

842a4b952b3a473d2182335e28373807dc42d325
jangerhard <jgschoepp@gmail.com>
2018-03-24 01:32:02 +0100

    Remove wrapper div from particles

--- src/components/Particles/ParticlesContainer.js
+++ src/components/Particles/ParticlesContainer.js
@@ -3,18 +3,16 @@ import Particles from 'react-particles-js';
 import config from './config.json'
 
 const ParticlesContainer = () => (
-    <div>
-        <Particles
-            params={config}
-            style={{
-                position: "absolute",
-                top: 0,
-                left: 0,
-                minHeight: "100vh",
-                zIndex: -1
-            }}
-        />
-    </div>
+    <Particles
+        params={config}
+        style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            minHeight: "100vh",
+            zIndex: -1
+        }}
+    />
 );
 
 export default ParticlesContainer;

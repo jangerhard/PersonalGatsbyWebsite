5b6269cc94a80e35d1e570473446f43b71f75fb3
jangerhard <jgschoepp@gmail.com>
2018-03-25 14:47:58 +0200

    Ad console to particles

--- src/components/Particles/ParticlesContainer.js
+++ src/components/Particles/ParticlesContainer.js
@@ -2,17 +2,20 @@ import React from "react";
 import Particles from 'react-particles-js';
 import config from './config.json'
 
-const ParticlesContainer = () => (
-    <Particles
-        params={config}
-        style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            minHeight: "100vh",
-            zIndex: -1
-        }}
-    />
-);
+const ParticlesContainer = () => {
+    console.log("Render Particles");
+    return (
+        <Particles
+            params={config}
+            style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                minHeight: "100vh",
+                zIndex: -1
+            }}
+        />
+    )
+};
 
 export default ParticlesContainer;

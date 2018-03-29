4f5503df521589f31d6c0399cee76c587c29bd13
jangerhard <jgschoepp@gmail.com>
2018-03-29 20:36:50 +0200

    Fix layout for all browsers

--- src/components/Pages/Education/EducationShowcase.js
+++ src/components/Pages/Education/EducationShowcase.js
@@ -9,30 +9,32 @@ class EducationShowcase extends React.Component {
     render() {
         return (
             <div className={styles.page}>
-                <SchoolShowcase
-                    name={"Pace University"}
-                    title={"Master of Computer Science"}
-                    location={"New York City, USA"}
-                    gpa={3.86}
-                    imgSrc={paceLogo}
-                    year={2017}
-                    month={"May"}
-                />
-                <SchoolShowcase
-                    name={"Hong Kong Polytechnic University"}
-                    title={"Exchange Year"}
-                    location={"Hung Hom, Hong Kong"}
-                    imgSrc={polyULogo}
-                    year={2013}
-                />
-                <SchoolShowcase
-                    name={"University of Stavanger"}
-                    title={"Bachelor of Computer Engineering"}
-                    location={"Stavanger, Norway"}
-                    imgSrc={uisLogo}
-                    year={2014}
-                    month={"May"}
-                />
+                <div className={styles.grid}>
+                    <SchoolShowcase
+                        name={"Pace University"}
+                        title={"Master of Computer Science"}
+                        location={"New York City, USA"}
+                        gpa={3.86}
+                        imgSrc={paceLogo}
+                        year={2017}
+                        month={"May"}
+                    />
+                    <SchoolShowcase
+                        name={"Hong Kong Polytechnic University"}
+                        title={"Exchange Year"}
+                        location={"Hung Hom, Hong Kong"}
+                        imgSrc={polyULogo}
+                        year={2013}
+                    />
+                    <SchoolShowcase
+                        name={"University of Stavanger"}
+                        title={"Bachelor of Computer Engineering"}
+                        location={"Stavanger, Norway"}
+                        imgSrc={uisLogo}
+                        year={2014}
+                        month={"May"}
+                    />
+                </div>
             </div>
         )
     }

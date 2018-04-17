eacda167914ada7b1f62134c00bc2013496c27c4
jangerhard <jgschoepp@gmail.com>
2018-04-17 19:58:54 +0200

    Require env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,5 @@
-let activeEnv = process.env.ACTIVE_ENV;
-
-if (!activeEnv) {
-    activeEnv = 'development';
-}
-
 require('dotenv').config({
-    path: `.env.${activeEnv}`
+    path: `.env`
 });
 
 module.exports = {

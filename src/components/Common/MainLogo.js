63c5423e955f0b5064d2abe556d75d568d9f849a
jangerhard <jgschoepp@gmail.com>
2018-05-03 20:38:07 +0200

    Use shorthand for components

--- src/components/Common/MainLogo.js
+++ src/components/Common/MainLogo.js
@@ -2,16 +2,12 @@ import React from "react";
 import PropTypes from 'prop-types'
 import style from './MainLogo.module.css'
 
-class MainLogo extends React.Component {
-    render() {
-        return (
-            <div className={`${style.logoCircle} mainLogo`} style={{
-                width: this.props.size,
-                height: this.props.size
-            }} />
-        )
-    }
-}
+const MainLogo = ({size}) => (
+    <div className={`${style.logoCircle} mainLogo`} style={{
+        width: size,
+        height: size
+    }}/>
+);
 
 MainLogo.propTypes = {
     size: PropTypes.number,
